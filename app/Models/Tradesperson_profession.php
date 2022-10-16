245f2c1ea023309c023c3c733490b547235c7bb5
matyiii <mezofimate@gmail.com>
2022-10-16 17:32:37 +0200

    Tradesperson_profession model

---
+++ app/Models/Tradesperson_profession.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tradesperson_profession extends Model
+{
+    use HasFactory;
+}
