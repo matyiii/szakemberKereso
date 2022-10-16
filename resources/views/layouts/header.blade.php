83b3bfab6f0e720a1e606d4054c4543c31501e77
matyiii <mezofimate@gmail.com>
2022-10-16 20:31:17 +0200

    header balde

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg">
-    <img id="szakilogo" src="{{ URL::to('/szakilogo.jpg') }}">
-    <a class="navbar-brand">SzakemberKereső</a>
+<nav class="navbar navbar-expand-lg" id="navbar">
+    <a href="#"><img id="szakilogo" src="{{ URL::to('/szakilogo.jpg') }}"></a>
+    <a class="navbar-brand" href="#">SzakemberKereső</a>
 </nav>
