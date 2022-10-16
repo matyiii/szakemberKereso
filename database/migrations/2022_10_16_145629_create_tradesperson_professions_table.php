7fa30e55d3819ad90ad22b6585cfb7ad96058e1a
matyiii <mezofimate@gmail.com>
2022-10-16 17:32:25 +0200

    added create tradespersons_professions table to migration

---
+++ database/migrations/2022_10_16_145629_create_tradesperson_professions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tradesperson_professions', function (Blueprint $table) {
+            $table->integer('tradesperson_id');
+            $table->integer('profession_id');
+            $table->foreign('tradesperson_id')->references('id')->on('tradespersons')->onDelete('cascade');
+            $table->foreign('profession_id')->references('id')->on('professions')->onDelete('cascade');
+            //$table->foreignId('tradesperson_id')->constrained('tradespersons');
+            //$table->foreignId('profession_id')->constrained('professions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tradesperson_professions');
+    }
+};
