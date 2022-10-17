4f9ebf4a5781628faf00a76feed60b0870b0d43e
matyiii <mezofimate@gmail.com>
2022-10-17 11:37:24 +0200

    navbar collapse working, added nav-items

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,18 @@
 <nav class="navbar navbar-expand-lg" id="navbar">
     <a href="#"><img id="szakilogo" src="{{ URL::to('/szakilogo.jpg') }}"></a>
     <a class="navbar-brand" href="#">SzakemberKereső</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="navbar-link" href="#">Szakember Hozzáadás</a>
+            </li>
+            <li class="nav-item">
+                <a class="navbar-link" href="#">Szakember Lista</a>
+            </li>
+        </ul>
+    </div>
 </nav>
