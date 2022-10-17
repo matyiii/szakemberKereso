997c59e84f94e1a12bf510158e4575f44592ef1b
matyiii <mezofimate@gmail.com>
2022-10-17 17:53:31 +0200

    added rating to tradesperson table

--- database/migrations/2022_10_16_142050_create_tradespeople_table.php
+++ database/migrations/2022_10_16_142050_create_tradespeople_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->tinyInteger('highlighted');
             $table->date('startDate');
             $table->date('endDate');
+            $table->integer('ratingSum');
+            $table->integer('ratingCount');
         });
     }
 
