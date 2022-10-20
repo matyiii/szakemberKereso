ab42055ec6beb92cfd5feb53ab8d5be67ebcc92e
matyiii <mezofimate@gmail.com>
2022-10-20 11:21:39 +0200

    TradespersonController added with index function, returns all

---
+++ app/Http/Controllers/TradespersonController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tradesperson;
+use Illuminate\Http\Request;
+
+class TradespersonController extends Controller
+{
+    public function index(){
+        $tradespersons = Tradesperson::all();
+        //dd($tradespersons);
+        return view('welcome')->with('tradespersons',$tradespersons);
+    }
+}
