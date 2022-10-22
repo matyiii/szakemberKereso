9a9f33bcc1b872ea534bb3725e9f3620eb441659
matyiii <mezofimate@gmail.com>
2022-10-22 22:04:05 +0200

    new function addTradesperson

--- app/Http/Controllers/TradespersonController.php
+++ app/Http/Controllers/TradespersonController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profession;
 use App\Models\Tradesperson;
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class TradespersonController extends Controller
@@ -11,5 +13,30 @@ class TradespersonController extends Controller
         $tradespersons = Tradesperson::all();
         //dd($tradespersons);
         return view('home')->with('tradespersons',$tradespersons);
+    }
+
+    public function addTradesperson(Request $request){
+        $validatedData = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'zip' => 'required|min_digits:4',
+            'city' => 'required',
+            'trade' => 'required',
+        ]);
+        //dd($validatedData);
+        $tradespersons = new Tradesperson();
+        $addresses = new Address();
+        $professions = new Profession();
+
+        $tradespersons->firstname = $validatedData['firstname'];
+        $tradespersons->lastname = $validatedData['lastname'];
+        $tradespersons->save();
+
+        $professions->name = $validatedData['trade'];
+        $tradespersons->professionsTp()->save($professions);
+
+        $addresses->zipcode = $validatedData['zip'];
+        $addresses->city = $validatedData['city'];
+        $tradespersons->addressTp()->save($addresses);
     }
 }
