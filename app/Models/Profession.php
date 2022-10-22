852e25db055ce3babbc2660d4007fd498c09c31a
matyiii <mezofimate@gmail.com>
2022-10-22 22:03:15 +0200

    turn off defaultt timestamp columns, rename table

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profession extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     protected $fillable=['name'];
 
     public function tradespersonProfession(){
         //return $this->belongsToMany(Tradesperson::class);
-        return $this->belongsToMany(Tradesperson::class,'Tradesperson_profession','tradesperson_id','profession_id');
+        return $this->belongsToMany(Tradesperson::class,'tradesperson_professions','tradesperson_id','profession_id');
     }
 }
