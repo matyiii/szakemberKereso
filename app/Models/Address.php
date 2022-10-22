f4d12858cd52ec9314e6a45997a499f2f157cfea
matyiii <mezofimate@gmail.com>
2022-10-22 22:03:36 +0200

    class renamed, ignore default timestamp column

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Addresses extends Model
+class Address extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
     protected $fillable=['zipcode','city'];
 
     public function tradespersonAddress(){
