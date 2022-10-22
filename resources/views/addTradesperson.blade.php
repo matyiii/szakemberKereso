35dd1895dbc051f402a8c2f39273ff6dfb7a4ccb
matyiii <mezofimate@gmail.com>
2022-10-22 22:01:22 +0200

    added default values to test, show invalid input errors

--- resources/views/addTradesperson.blade.php
+++ resources/views/addTradesperson.blade.php
@@ -5,15 +5,15 @@
         <form method="POST">
             @csrf
             <label for="firstname">Firstname:</label>
-            <input type="text" id="firstname" name="firstname" class="form-control">
+            <input type="text" id="firstname" name="firstname" class="form-control" value="tesztfirstname">
             <label for="lastname">Lastname:</label>
-            <input type="text" id="lastname" name="lastname" class="form-control">
+            <input type="text" id="lastname" name="lastname" class="form-control" value="tesztlastname">
             <label for="zip">ZIP:</label>
-            <input type="text" id="zip" name="zip" class="form-control">
+            <input type="text" id="zip" name="zip" class="form-control" value="1234">
             <label for="city">City:</label>
-            <input type="text" id="city" name="city" class="form-control">
+            <input type="text" id="city" name="city" class="form-control" value="tesztcity">
             <label for="trade">Trade:</label>
-            <input type="text" id="trade" name="trade" class="form-control">
+            <input type="text" id="trade" name="trade" class="form-control" value="teszttrade">
             <label for="prictures">Pictures:</label>
             <input type="file" id="prictures" name="prictures" class="form-control" multiple>
             <br>
@@ -22,5 +22,15 @@
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </form>
+        @if ($errors->any())
+            <br>
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 @endsection
