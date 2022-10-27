4831bce213bdc2cef84a5a6fe71925b2f3453f13
matyiii <mezofimate@gmail.com>
2022-10-27 19:29:27 +0200

    add/remove trade input

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+$(document).ready(function () {
+    var tradeCount = 1
+    $("#addTrade").on("click", function () {
+        $("#dynamicAdd").append(`<input type="text" id="trade${++tradeCount}" name="trade" class="form-control" placeholder="Trade${tradeCount}">`);
+    })
+
+    $("#removeTrade").on("click", function () {
+        if(tradeCount>1){
+            $("#dynamicAdd input").last().remove();
+            tradeCount--;
+        }
+    })
+});
