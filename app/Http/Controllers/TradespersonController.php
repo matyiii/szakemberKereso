c39e2093c2d8656bd1ebfd6d245225824bbfc52f
matyiii <mezofimate@gmail.com>
2022-10-27 14:16:01 +0200

    insert addressId foreignkey

--- app/Http/Controllers/TradespersonController.php
+++ app/Http/Controllers/TradespersonController.php
@@ -9,18 +9,20 @@ use Illuminate\Http\Request;
 
 class TradespersonController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $tradespersons = Tradesperson::all()->take(10);
         //dd($tradespersons);
         $highlightedTradespersons = $this->listHighlightedTps();
-        return view('home')->with('tradespersons',$tradespersons)->with('highlighted',$highlightedTradespersons);
+        return view('home')->with('tradespersons', $tradespersons)->with('highlighted', $highlightedTradespersons);
     }
 
-    public function addTradesperson(Request $request){
+    public function addTradesperson(Request $request)
+    {
         $validatedData = $request->validate([
             'firstname' => 'required',
             'lastname' => 'required',
-            'zip' => 'required|min_digits:4',
+            'zip' => 'required|min_digits:4|max_digits:5',
             'city' => 'required',
             'trade' => 'required',
         ]);
@@ -30,40 +32,28 @@ class TradespersonController extends Controller
         $address = new Address();
         $profession = new Profession();
 
-        $tradesperson = Tradesperson::create([
-            'firstname' =>$validatedData['firstname'],
-            'lastname' => $validatedData['lastname'],
-            //'addressId' => '1',
-            'highlighted' => is_null($request->input('highlighted')) ? '0':'1',
-        ]);
-
         $address = Address::firstOrCreate([
             'zipcode' => $validatedData['zip'],
             'city' => $validatedData['city'],
-            //'tradesperson_id' => $tradesperson->addressTp()->save($address)
+        ]);
+
+        $tradesperson = Tradesperson::create([
+            'firstname' => $validatedData['firstname'],
+            'lastname' => $validatedData['lastname'],
+            //'addressId' => $tradesperson->addressTp()->save($address),
+            'addressId' => Address::where('zipcode', $validatedData['zip'])->where('city', $validatedData['city'])->pluck('id')->first(),
+            'highlighted' => is_null($request->input('highlighted')) ? '0' : '1',
         ]);
 
         $profession = Profession::create([
             'name' => $validatedData['trade'],
         ]);
-        dd([$tradesperson,$address,$profession]);
-
-/*         $tradesperson->firstname = $validatedData['firstname'];
-        $tradesperson->lastname = $validatedData['lastname'];
-        $tradesperson->highlighted = is_null($request->input('highlighted')) ? '0':'1';
-        $tradesperson->save();
-
-        $profession->name = $validatedData['trade'];
-        $tradesperson->professionsTp()->save($profession);
-
-        $address->zipcode = $validatedData['zip'];
-        $address->city = $validatedData['city'];
-        $tradesperson->addressTp()->save($address);
-        dd($address); */
+        dd([$tradesperson, $address, $profession]);
     }
 
-    public function listHighlightedTps(){
-        $data = Tradesperson::all()->where('highlighted',1);
+    public function listHighlightedTps()
+    {
+        $data = Tradesperson::all()->where('highlighted', 1);
 
         return $data;
     }
