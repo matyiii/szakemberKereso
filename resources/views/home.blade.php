7ed32cf3a44589ba5278fa03f746c398413e2237
matyiii <mezofimate@gmail.com>
2022-10-30 12:20:22 +0100

    added localization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,29 +3,30 @@
 @section('content')
     <div id="mainContent">
         <div class="col-3" id="left">
-            <h3>Highlighted tradepersons:</h3>
+            <h3>{{ __('Highlighted tradepersons:') }}</h3>
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <div class="card">
                             <img src="https://via.placeholder.com/200" class="d-block w-100" alt="tradespersonPlaceholder">
                             <div class="card-body">
-                                <h5 class="card-title">Firstname Lastname</h5>
-                                <p class="card-text">Trade</p>
-                                <p class="card-text">Introduction</p>
-                                <a href="#" class="btn btn-primary">Details</a>
+                                <h5 class="card-title">{{ __('Firstname Lastname') }}</h5>
+                                <p class="card-text">{{ __('Trade') }}</p>
+                                <p class="card-text">{{ __('Introduction') }}</p>
+                                <a href="#" class="btn btn-primary">{{ __('Details') }}</a>
                             </div>
                         </div>
                     </div>
                     @foreach ($highlighted as $person)
                         <div class="carousel-item">
                             <div class="card">
-                                <img src="https://via.placeholder.com/200" class="d-block w-100" alt="tradespersonPlaceholder">
+                                <img src="https://via.placeholder.com/200" class="d-block w-100"
+                                    alt="tradespersonPlaceholder">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $person->firstname }} {{ $person->lastname }}</h5>
                                     <p class="card-text">{{ $person->trade == '' ? 'No trade' : $person->trade }}</p>
                                     <p class="card-text">{{ $person->introduction }}</p>
-                                    <a href="#" class="btn btn-primary">Details</a>
+                                    <a href="#" class="btn btn-primary">{{ __('Details') }}</a>
                                 </div>
                             </div>
                         </div>
@@ -47,9 +48,9 @@
             <h1>TESZT</h1>
         </div>
         <div class="col-3" id="right">
-            <h3>All tradeperson:</h3>
-            @foreach ($tradespersons as $tp)
-                <p>Name: {{ $tp->firstname }} {{ $tp->lastname }}</p>
+            <h3>{{ __('Trades') }}:</h3>
+            @foreach ($trades as $trade)
+                <p>{{ $trade->name }}</p>
             @endforeach
         </div>
     </div>
