5123dda421051d900b932436173e0d3d2be7eace
matyiii <mezofimate@gmail.com>
2022-10-31 19:59:34 +0100

    fill modal based on clicked "id"

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
     var tradeCount = 1
     $("#addTrade").on("click", function () {
-        if(tradeCount!=3){
+        if (tradeCount != 3) {
             $("#dynamicAdd").append(`<input type="text" id="trade" name="trade${++tradeCount}" class="form-control" placeholder="Trade${tradeCount}">`);
         }
-    })
+    });
 
     $("#removeTrade").on("click", function () {
-        if(tradeCount>1){
+        if (tradeCount > 1) {
             $("#dynamicAdd input").last().remove();
             tradeCount--;
         }
-    })
+    });
+
+    $("#tradespersonList").on("click", ".openmodal", function () {
+        let traderpersonId = $(this).attr('data-id');
+    
+        if (traderpersonId > 0) {
+            $("#tradespersonInfo tbody").empty();
+            console.log("empty");
+            $.ajax({
+                type: 'GET',
+                url: `/getTradespersonData/${traderpersonId}`,
+                dataType: 'json',
+                success: function (response) {
+                    $("#tradespersonInfo tbody").html(response.html);
+                    $('#tradespersonModal').modal('show');
+                }
+            });
+        }
+    });
 });
