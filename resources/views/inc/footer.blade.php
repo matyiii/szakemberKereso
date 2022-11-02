efd0c2ab4ab65d7823a73c79714d4e20ef6d0bdf
matyiii <mezofimate@gmail.com>
2022-11-02 19:34:44 +0100

    refactor: new footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,3 +1,8 @@
-<div class="footer">
-    {{ __('Elérhetőségek') }}
-</div>
+<footer class="text-center text-lg-start bg-light text-muted fixed-bottom ">
+    <div class="text-center p-8" style="background-color: rgba(0, 0, 0, 0.05);">
+        <p class="text-left">
+            {{ __('Elérhetőségek') }}:
+        </p>
+        <a class="text-reset fw-bold" href="#" style="text-decoration: none">© 2022 Copyright:</a>
+    </div>
+</footer>
