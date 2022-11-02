34ee700d2dc994c28a0210fa2006fdb0e584d4ff
matyiii <mezofimate@gmail.com>
2022-11-02 19:34:34 +0100

    refactor: new navbar

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,24 +1,67 @@
-<nav class="navbar navbar-expand-lg" id="navbar">
-    <a href="{{ url('/') }}"><img id="szakilogo" src="{{ URL::to('/szakilogo.jpg') }}" alt="szakilogo"></a>
-    <a class="navbar-brand" href="{{ url('/') }}">{{ __('SzakemberKereső') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="navbar-link" href="addTp">{{ __('Szakember Hozzáadás') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="navbar-link" href="listAllTp">{{ __('Szakember Lista') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="navbar-link" href="#">{{ __('Belépés') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="navbar-link" href="#">{{ __('Regisztráció') }} </a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" id="navbar">
+    <div class="container" id="navContainer">
+        <a href="{{ url('/') }}"><img id="szakilogo" src="{{ URL::to('/szakilogo.jpg') }}"
+                alt="szakilogo"></a>
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ __('SzakemberKereső') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav me-auto">
+                
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="listAllTp">{{ __('Szakember Lista') }}</a>
+                    </li>
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="addTp">{{ __('Szakember Hozzáadás') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="listAllTp">{{ __('Szakember Lista') }}</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
