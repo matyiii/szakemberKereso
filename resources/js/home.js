07e1922a4a57232a8b44d6fcfbb9d8292b8b7721
matyiii <mezofimate@gmail.com>
2022-11-06 21:23:25 +0100

    added dselect on cityselect

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,7 +1,14 @@
 $(document.location.href == "http://127.0.0.1:8000/").ready(function () {
-    var tradeSelect = document.querySelector('#tradeSelect');
+    let tradeSelect = document.querySelector('#tradeSelect');
+    let citySelect = document.querySelector('#citySelect');
 
     dselect(tradeSelect, {
+        search: true,
+        creatable: false,
+        clearable: false,
+    });
+
+    dselect(citySelect, {
         search: true,
         creatable: false,
         clearable: false,
