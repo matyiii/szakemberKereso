dccc8c60c0d43ec24c0510686529839dbdb5315b
matyiii <mezofimate@gmail.com>
2022-11-09 21:46:33 +0100

    fix: load js on demand

--- resources/js/addTradeperson.js
+++ resources/js/addTradeperson.js
@@ -1,33 +1,35 @@
-$(window.location.href == "/addTp").ready(function () {
-    var tradeCount = 1
-    $("#addTrade").on("click", function () {
-        if (tradeCount != 3) {
-            $("#dynamicAdd").append(`<input type="text" id="trade" name="trade${++tradeCount}" class="form-control" placeholder="Trade${tradeCount}">`);
-        }
-    });
+$(document).ready(function () {
+    if (window.location.href == "/addTp") {
+        var tradeCount = 1
+        $("#addTrade").on("click", function () {
+            if (tradeCount != 3) {
+                $("#dynamicAdd").append(`<input type="text" id="trade" name="trade${++tradeCount}" class="form-control" placeholder="Trade${tradeCount}">`);
+            }
+        });
 
-    $("#removeTrade").on("click", function () {
-        if (tradeCount > 1) {
-            $("#dynamicAdd input").last().remove();
-            tradeCount--;
-        }
-    });
+        $("#removeTrade").on("click", function () {
+            if (tradeCount > 1) {
+                $("#dynamicAdd input").last().remove();
+                tradeCount--;
+            }
+        });
 
-    $("#tradespersonList").on("click", ".openmodal", function () {
-        let traderpersonId = $(this).attr('data-id');
+        $("#tradespersonList").on("click", ".openmodal", function () {
+            let traderpersonId = $(this).attr('data-id');
 
-        if (traderpersonId > 0) {
-            $("#tradespersonInfo tbody").empty();
-            console.log("empty");
-            $.ajax({
-                type: 'GET',
-                url: `/getTradespersonData/${traderpersonId}`,
-                dataType: 'json',
-                success: function (response) {
-                    $("#tradespersonInfo tbody").html(response.html);
-                    $('#tradespersonModal').modal('show');
-                }
-            });
-        }
-    });
+            if (traderpersonId > 0) {
+                $("#tradespersonInfo tbody").empty();
+                console.log("empty");
+                $.ajax({
+                    type: 'GET',
+                    url: `/getTradespersonData/${traderpersonId}`,
+                    dataType: 'json',
+                    success: function (response) {
+                        $("#tradespersonInfo tbody").html(response.html);
+                        $('#tradespersonModal').modal('show');
+                    }
+                });
+            }
+        });
+    }
 });
--- resources/js/home.js
+++ resources/js/home.js
@@ -1,16 +1,18 @@
-$(document.location.href == "http://127.0.0.1:8000/").ready(function () {
-    let tradeSelect = document.querySelector('#tradeSelect');
-    let citySelect = document.querySelector('#citySelect');
+$(document).ready(function () {
+    if (window.location.href == "http://127.0.0.1:8000/") {
+        let tradeSelect = document.querySelector('#tradeSelect');
+        let citySelect = document.querySelector('#citySelect');
 
-    dselect(tradeSelect, {
-        search: true,
-        creatable: false,
-        clearable: false,
-    });
+        dselect(tradeSelect, {
+            search: true,
+            creatable: false,
+            clearable: false,
+        });
 
-    dselect(citySelect, {
-        search: true,
-        creatable: false,
-        clearable: false,
-    });
+        dselect(citySelect, {
+            search: true,
+            creatable: false,
+            clearable: false,
+        });
+    }
 })
