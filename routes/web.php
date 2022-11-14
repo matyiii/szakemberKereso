47d22099f58ba3d90121e4423401fbefc27d8aa5
matyiii <mezofimate@gmail.com>
2022-11-14 17:30:57 +0100

    feat: added getSearchData route

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth')->group(function () {
     #listAllTp view
     Route::get('/listAllTp', [ListAllTpController::class, 'listAllTp'])->withoutMiddleware('auth');
     Route::get('/getTradespersonData/{tradespersonId}', [ListAllTpController::class, 'getTradespersonData'])->name('getTradespersonData')->withoutMiddleware('auth');
+    Route::get('/getSearchedData',[ListAllTpController::class,'getSearchedData'])->name('getSearchedData')->withoutMiddleware('auth');
 
     #login view
     Route::get('/login', [HomeController::class, 'index'])->name('login');
