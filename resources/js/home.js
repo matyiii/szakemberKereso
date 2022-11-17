83b668beed8a27e9edac9a68f8a906239b1f9c5b
matyiii <mezofimate@gmail.com>
2022-11-17 11:25:28 +0100

    refactor: removed ajax

--- resources/js/home.js
+++ resources/js/home.js
@@ -7,23 +7,5 @@ $(window).ready(function () {
 
         $("#citySelect").select2({
         });
-
-        $("#searchBtn").on("click", function (e) {
-            e.preventDefault(); //teszteléshez
-            let selectedTrade = $("#tradeSelect").val();
-            let selectedCity = $("#citySelect").val();
-            $.ajax({
-                type: "GET",
-                url:`/getSearchedData`,
-                data:{
-                    selectedTrade : selectedTrade,
-                    selectedCity : selectedCity,
-                },
-                success: function(response){
-                    console.log(response);
-                    //window.location = "http://127.0.0.1:8000/listAllTp";
-                }
-            })
-        });
     }
 })
