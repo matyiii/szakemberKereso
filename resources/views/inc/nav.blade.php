7ec8cf438ad0dc7cfb0425268760d587963d582e
matyiii <mezofimate@gmail.com>
2022-11-28 09:23:01 +0100

    fix(nav): changed logout data-bs-toggle to data-toggle

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -44,7 +44,7 @@
                     </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
