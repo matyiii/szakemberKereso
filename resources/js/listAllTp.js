202f7577f5d8300a6ead050b559fc1ca1be2b4c0
matyiii <mezofimate@gmail.com>
2022-11-30 20:44:03 +0100

    refactor: rename variable

--- resources/js/listAllTp.js
+++ resources/js/listAllTp.js
@@ -1,14 +1,12 @@
 $(window).ready(function () {
     if (window.location.pathname.match(/listAllTp.*/)) {
         $("#tradespersonList").on("click", ".openmodal", function () {
-            let traderpersonId = $(this).attr('data-id');
-
-            if (traderpersonId > 0) {
+            let tradespersonId = $(this).attr('data-id');
+            if (tradespersonId > 0) {
                 $("#tradespersonInfo tbody").empty();
-                console.log("empty");
                 $.ajax({
                     type: 'GET',
-                    url: `/getTradespersonData/${traderpersonId}`,
+                    url: `/getTradespersonData/${tradespersonId}`,
                     dataType: 'json',
                     success: function (response) {
                         $("#tradespersonInfo tbody").html(response.html);
