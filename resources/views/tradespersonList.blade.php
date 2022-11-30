a995dbbf6212e445d2ec454c0351428bd786cc3e
matyiii <mezofimate@gmail.com>
2022-11-30 20:44:31 +0100

    feat: different modal for guest and loged in users

--- resources/views/tradespersonList.blade.php
+++ resources/views/tradespersonList.blade.php
@@ -27,6 +27,7 @@
         </div>
     </div>
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" id="tradespersonModal">
+        @guest
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -40,9 +41,27 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button> {{-- Admin bejelentkezésel update --}}
                 </div>
             </div>
         </div>
+        @else
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="w-100" id="tradespersonInfo">
+                        <tbody></tbody>
+                     </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button id="deleteBtn" type="button" class="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+        @endguest
     </div>
 @endsection
