b1f96c13a5762bdbf7e7aabfcc8e5809adf722ea
matyiii <mezofimate@gmail.com>
2022-12-01 11:20:34 +0100

    feat(Test): RegisterPageTest added, check if page can be rendered, register a new user

---
+++ tests/Feature/RegisterPageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterPageTest extends TestCase
+{
+    public function test_registration_screen_can_be_rendered()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_new_users_can_register()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Teszt Elek',
+            'email' => 'tesztelek@uni-eszterhazy.hu',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ]);
+ 
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+}
